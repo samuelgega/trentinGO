b037b1f182f8587e9477e65227bb439be32d7366
FrancescoRad03 <ioanfrancesco@unitn.it>
2026-05-08 17:26:18 +0200

    Modifica file config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URL);
-      console.log('Connessione a MongoDB riuscita');
+      console.log('Connessione al Database riuscita');
   } catch (err) {
     console.error('Errore connessione MongoDB:', err.message);
   }
