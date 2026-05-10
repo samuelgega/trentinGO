c1cb8da0a15ba97471075072f51267220a9f1bb3
FrancescoRad03 <ioanfrancesco@unitn.it>
2026-05-10 03:47:29 +0200

    Aggiunta modello Evento

---
+++ backend/models/Evento.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const schemaEvento = new mongoose.Schema({
+    type: String,
+    geometry: {
+        type: { type: String, enum: ["Point"] },
+        coordinates: [Number]
+    },
+    properties: {
+        nome: {
+            type: String,
+            required: [true, 'Nome obbligatorio'],
+            minlength: 2,
+            maxlength: 100,
+            unique: true,
+        },
+        descrizione: {
+            type: String,
+            required: false
+        },
+        categoria: String,
+        prezzo: Number,
+        punteggio: Number,
+        immagine: [String],
+        dataInizio: Date,
+        dataFine: Date,
+        gestore: String,
+        dataCreazione: Date.now
+    }
+})
+
+module.exports = mongoose.model('Evento', schemaEvento, 'Eventi')
