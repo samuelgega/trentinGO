13030b7cc7741e0b11e36b8bb3d7c266b654b4ac
FrancescoRad03 <ioanfrancesco@unitn.it>
2026-05-10 03:40:29 +0200

    Creazione file models/PDI.js

---
+++ backend/models/PDI.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const schemaPDI = new mongoose.Schema({
+    type: String,
+    geometry: {
+        type: { type: String, enum: ["Point"] },
+        coordinates: [Number]
+    },
+    properties: {
+        nome: {
+            type: String,
+            required: [true, "Nome obbligatorio"],
+            minlength: 2,
+            maxlength: 100,
+            unique: true
+        },
+        descrizione: {
+            type: string,
+            required: false
+        },
+        categoria: String,
+        prezzo: Number,
+        punteggio: Number,
+        immagine: [String]
+    }
+})
+
+module.exports = mongoose.model('PDI', schemaPDI, 'PDI')
--- backend/scripts/seedPDI.js
+++ backend/scripts/seedPDI.js
@@ -1,4 +1,5 @@
 /* Script per generare i PDI all'interno del database */
+const mongoose = require('mongoose')
 
 // Connessione al Database
 const connectDB = require('../config/db')
@@ -7,32 +8,10 @@ connectDB()
 // Prelevare il file .json contente i PDI
 const fs = require("fs");
 const jsonText = fs.readFileSync("./backend/data/trentino_pdi.json", "utf8");
-console.log(jsonText);
 const trentinoPDI = JSON.parse(jsonText).features;
 
-// Definizione Schema
-const mongoose = require('mongoose')
-const schemaPDI = new mongoose.Schema({
-    type: String,
-    geometry: {
-        type: { type: String, enum: ["Point"] },
-        coordinates: [Number]
-    },
-    properties: {
-        id: Number,
-        nome: String,
-        descrizione: String,
-        latitudine: Number,
-        longitudine: Number,
-        categoria: String,
-        prezzo: Number,
-        punteggio: Number,
-        immagine: String
-    }
-})
-
-// Definizione Oggetto PDI
-const PDI = mongoose.model("PDI", schemaPDI, "PDI") // (nomeOggetto, schema, nomeCollection)
+// Importare Schema
+const PDI = require('../models/PDI')
 
 // Comando per inserire tutti i PDI su MongoDB
 // PDI.insertMany(trentinoPDI)
