96d7dd77cd9f51ba7550348cf3a6c5b12a6df93a
C4ppe <marcocappe2004@gmail.com>
2026-05-14 14:51:10 +0200

    Aggiunga rotta per homepage

---
+++ frontend/src/pages/home/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>In fase di sviluppo</p>
+    </div>
+  );
+};
+
+export default Homepage;
