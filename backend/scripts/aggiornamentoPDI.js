3c9a4ef14ec3e6e4ed39de7a3d263de48e8c257f
C4ppe <marcocappe2004@gmail.com>
2026-05-20 16:27:56 +0200

    Modifica alla script per aggiungere la descrizione

--- backend/scripts/aggiornamentoPDI.js
+++ backend/scripts/aggiornamentoPDI.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose')
 const connectDB = require('../config/db')
-const fs = require("fs");
+const path = require("path");
+const fs = require('fs');
 const PDI = require('../models/PDI')
-const aggiornamenti = require('./aggiornamenti.json');
+const aggiornamenti = require('../data/pdi_descrizioni.json');
 
 const popolaDatabase = async () => {
     try {
 
-        connectDB();
+        await connectDB();
         console.log("lettura file json");
-        const rawData = fs.readFileSync('../data/pdi_descrizioni.json', 'utf-8');
+        const jsonPath = path.join(__dirname, '../data/pdi_descrizioni.json');
+        const rawData = fs.readFileSync(jsonPath, 'utf-8');
         console.log("file letto correttamente");
         const aggiornamenti = JSON.parse(rawData);
         let aggiornati = 0;
@@ -35,10 +36,7 @@ const popolaDatabase = async () => {
 
     } catch (error) {
         console.error("Errore durante l'aggiornamento:", error);
-    } finally {
-        mongoose.connection.close();
-        process.exit();
     }
 };
 
-//export default popolaDatabase;
+//popolaDatabase();
