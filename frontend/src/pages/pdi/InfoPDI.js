b72e7daffc37e66d9c9c21451de0c00d457611c0
C4ppe <marcocappe2004@gmail.com>
2026-05-20 16:10:20 +0200

    aggiunta rotta per la pagina specifica di un PDI

---
+++ frontend/src/pages/pdi/InfoPDI.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+
+const InfoPDI = () => {
+
+    const { id } = useParams();
+    
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default InfoPDI;
