ccd4375eecada7276a2953e78e9235ebc7454f27
C4ppe <marcocappe2004@gmail.com>
2026-05-20 15:20:59 +0200

    Aggiunto script per aggiungere la descrizione ai PDI

--- backend/scripts/aggiornamentoPDI.js
+++ backend/scripts/aggiornamentoPDI.js
@@ -1,31 +1,7 @@
-/* Script per generare i PDI all'interno del database */
 const mongoose = require('mongoose')
-
-// Connessione al Database
 const connectDB = require('../config/db')
-connectDB()
-
-// Prelevare il file .json contente i PDI
 const fs = require("fs");
-const jsonText = fs.readFileSync("./backend/data/trentino_pdi.json", "utf8");
-const trentinoPDI = JSON.parse(jsonText).features;
-
-// Importare Schema
 const PDI = require('../models/PDI')
-
-// Comando per inserire tutti i PDI su MongoDB
-// PDI.insertMany(trentinoPDI)
-//     .then(() => console.log("Tutti i PDI sono stati inseriti"))
-//     .catch(err => console.error("Errore:", err));
-
-
-// Comando per eliminare tutti i PDI dal database
-// PDI.deleteMany({})
-//     .then(() => console.log("Tutti i PDI sono stati eliminati"))
-//     .catch(err => console.error("Errore:", err));
-
-//scrip per popolare i PDI con una descrizione
-
 const aggiornamenti = require('./aggiornamenti.json');
 
 const popolaDatabase = async () => {
@@ -60,11 +36,9 @@ const popolaDatabase = async () => {
     } catch (error) {
         console.error("Errore durante l'aggiornamento:", error);
     } finally {
-        // Chiude la connessione e termina lo script
         mongoose.connection.close();
         process.exit();
     }
 };
 
-// Avvia la funzione
-popolaDatabase();
+//export default popolaDatabase;
