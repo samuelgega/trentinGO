3bd6bdb02d65761bc7b324c2cfdf78323c0d4f83
FrancescoRad03 <ioanfrancesco.rad@studenti.unitn.it>
2026-05-24 17:27:02 +0200

    Aggiunta api autenticazione gestore

--- backend/routes/gestoreRoutes.js
+++ backend/routes/gestoreRoutes.js
@@ -9,4 +9,7 @@ router.get("/", Gestore.visualizzaGestori);
 //route per la registrazione di un nuovo gestore
 router.post("/registrazione", Gestore.registrazioneGestore);
 
+//route per il login di un gestore
+router.post("/login", Gestore.loginGestore);
+
 module.exports = router;
